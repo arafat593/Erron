8e6f7e4f68131e0229fad946903fdde85151ca92
Md. Al Arafat <arafathossain593@gmail.com>
2025-12-14 09:33:25 +0600

    add theme

---
+++ lib/app/data/theme.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'app_colors.dart';
+import 'app_text_styles.dart';
+
+ThemeData themeData() {
+  return ThemeData(
+    scaffoldBackgroundColor: AppColors.whiteColor,
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      backgroundColor: AppColors.whiteColor,
+      elevation: 0,
+      selectedLabelStyle: AppTextStyles.medium12,
+      unselectedLabelStyle: AppTextStyles.medium12,
+    ),
+    appBarTheme: AppBarTheme(
+      backgroundColor: AppColors.whiteColor,
+      iconTheme: IconThemeData(color: AppColors.whiteColor),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(),
+    ),
+    outlinedButtonTheme: OutlinedButtonThemeData(
+      style: OutlinedButton.styleFrom(),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      border: OutlineInputBorder(),
+      enabledBorder: OutlineInputBorder(),
+      focusedBorder: OutlineInputBorder(),
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'package:get/get.dart';
-
-import 'app/routes/app_pages.dart';
-
-void main() {
-  runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-    ),
-  );
+import 'management.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const Management());
 }
