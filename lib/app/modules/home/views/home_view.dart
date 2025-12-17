46470519c024e398fb6f8e3274a67d09c7bf9d5d
Md. Al Arafat <arafathossain593@gmail.com>
2025-12-17 18:20:27 +0600

    home is working

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,7 +1,12 @@
+import 'dart:ui';
+
+import 'package:errone/app/data/app_text_styles.dart';
 import 'package:errone/app/data/image_path.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:get/get_connect/http/src/utils/utils.dart';
+import '../../../data/app_colors.dart';
 import '../../common_widgets/custom_app_bar.dart';
 import '../controllers/home_controller.dart';
 
@@ -32,6 +37,129 @@ class HomeView extends GetView<HomeController> {
             ),
           ),
         ],
+      ),
+
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.w),
+        child: Column(
+          children: [
+            // buttons
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 22.h),
+              child: Row(
+                children: [
+                  ElevatedButton(onPressed: () {}, child: Text('All')),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16.w),
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      child: Text('Free'),
+                    ),
+                  ),
+                  OutlinedButton(onPressed: () {}, child: Text('Paid')),
+                ],
+              ),
+            ),
+
+            // GridView
+            Expanded(
+              child: GridView.builder(
+                shrinkWrap: true,
+                primary: false,
+                physics: BouncingScrollPhysics(),
+                padding: EdgeInsets.zero,
+                itemCount: 20,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  // childAspectRatio: .9,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                  mainAxisExtent: 300,
+                ),
+                itemBuilder: (context, index) {
+                  return Container(
+                    // height: 100,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: AppColors.whiteColor,
+                      borderRadius: BorderRadius.circular(24.sp),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.all(10.sp),
+                      child: Column(
+                        children: [
+                          // top button
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              SizedBox(
+                                height: 20.h,
+                                width: 35.w,
+                                child: ElevatedButton(
+                                  onPressed: () {},
+                                  style: ElevatedButton.styleFrom(
+                                    textStyle: AppTextStyles.regular10,
+                                    backgroundColor: AppColors.pink,
+                                    foregroundColor: AppColors.whiteLight,
+                                    padding: EdgeInsets.symmetric(vertical: 0),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(
+                                        64.sp,
+                                      ),
+                                    ),
+                                  ),
+                                  child: Text("Live"),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 20.h,
+                                width: 35.w,
+                                child: ElevatedButton(
+                                  onPressed: () {},
+                                  style: ElevatedButton.styleFrom(
+                                    textStyle: AppTextStyles.regular10,
+                                    backgroundColor: AppColors.primaryColor,
+                                    foregroundColor: AppColors.whiteLight,
+                                    padding: EdgeInsets.symmetric(vertical: 0),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(
+                                        64.sp,
+                                      ),
+                                    ),
+                                  ),
+                                  child: Text("4k"),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 20.h,
+                                width: 35.w,
+                                child: ElevatedButton(
+                                  onPressed: () {},
+                                  style: ElevatedButton.styleFrom(
+                                    textStyle: AppTextStyles.regular10,
+                                    backgroundColor: AppColors.yellow,
+                                    foregroundColor: AppColors.whiteLight,
+                                    padding: EdgeInsets.symmetric(vertical: 0),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(
+                                        64.sp,
+                                      ),
+                                    ),
+                                  ),
+                                  child: Text("50"),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
